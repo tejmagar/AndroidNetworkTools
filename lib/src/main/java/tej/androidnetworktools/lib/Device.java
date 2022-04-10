68417f1279a9b86e2996b6321a18b525856c2aa1
Tej Magar <tejmagar20510@gmail.com>
2022-04-10 11:21:26 +0545

    updated default value

--- lib/src/main/java/tej/androidnetworktools/lib/Device.java
+++ lib/src/main/java/tej/androidnetworktools/lib/Device.java
@@ -1,8 +1,10 @@
 package tej.androidnetworktools.lib;
 
+import tej.androidnetworktools.lib.parsers.DeviceInfo;
+
 public class Device {
     public String hostname;
     public String ipAddress;
-    public String macAddress;
-    public String vendorName;
+    public String macAddress = DeviceInfo.UNKNOWN;
+    public String vendorName = DeviceInfo.UNKNOWN;
 }
